25d69493ee2c48477fb1dd17137025b0884bc0e1
Revital Elishkov <revitalrle@gmail.com>
2021-10-27 13:10:22 +0300

    add basic css,js,index,imgs

---
+++ js/meme-service.js
@@ -0,0 +1,19 @@
+
+var gKeywords = {'VIP': 1,'funny puk': 1}
+
+var gImages = [
+    {id: 1, url: 'img/1.jpg', keywords: ['VIP']}
+]
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+    {
+    txt: 'I never eat Falafel',
+    size: 20,
+    align: 'left',
+    color: 'red'
+    }
+    ]
+   }
