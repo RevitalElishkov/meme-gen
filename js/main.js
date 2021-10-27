12797c18e7318a009242f391c652a40e136a3f66
Revital Elishkov <revitalrle@gmail.com>
2021-10-27 14:12:47 +0300

    add canvas and img

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var gElCanvas;
+var gCtx;
+
+
+function onInit(){
+    console.log('ready')
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    drawImage();
+}
+
+
+function drawImage() {
+    console.log('hi');
+    var img = new Image();
+    img.src= getImgSrc();
+    img.onload = () => {
+      gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    };
+  }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,19 +1,31 @@
 
-var gKeywords = {'VIP': 1,'funny puk': 1}
+var gKeywords = { 'VIP': 1, 'funny puk': 1 }
 
 var gImages = [
-    {id: 1, url: 'img/1.jpg', keywords: ['VIP']}
+    { id: 1, url: 'imgs/meme-imgs/1.jpg', keywords: ['VIP'] }
 ]
 
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [
-    {
-    txt: 'I never eat Falafel',
-    size: 20,
-    align: 'left',
-    color: 'red'
-    }
+        {
+            txt: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
     ]
-   }
+}
+
+
+function getImgSrc() {
+    var imIdx = _getImgIdx(1)
+    var selectedImg = gImages[imIdx].url
+    // console.log('selectedImg', selectedImg); 
+    return selectedImg
+}
+
+function _getImgIdx(imgId) {
+    return gImages.findIndex(img => img.id === imgId)
+}
